6f312b8095e27f8fd2777ff0a5dc439741f92f4c
Brad Nelson <bnelson13@gmail.com>
2020-12-11 14:20:57 -0800

    Completed stopwatch

--- app2.js
+++ app2.js
@@ -5,8 +5,8 @@ const lapButton = document.querySelector('#stopwatch-button-lap');
 const stopwatchLaps = document.querySelector('.stopwatch-laps');
 const stopwatchTime = document.querySelector('#stopwatch-time');
 let timerInerval;
-lapTimes = [];
-lapCount = 1;
+let lapTimes = [];
+let lapCount = 1;
 let timeNumber = 0;
 let lapTimeNumber = 0;
 
@@ -15,6 +15,7 @@ startButton.addEventListener('click', function startTime() {
   stopButton.style.display = 'block';
   resetButton.style.display = 'none';
   lapButton.style.display = 'block';
+  document.querySelector('.stopwatch-laps').style.color = 'white';
   timerInerval = setInterval(() => {
     stopwatchTime.innerHTML = msToTime(10 * timeNumber++);
     document.querySelector(`#lap-time-${lapCount}`).innerHTML = msToTime(10 * lapTimeNumber++);
@@ -31,32 +32,35 @@ stopButton.addEventListener('click', function stopTime() {
 
 resetButton.addEventListener('click', function resetStopwatch() {
   lapCount = 1;
+  lapTimes = [];
   stopwatchTime.innerHTML = '00:00:00.00';
   timeNumber = 0;
   stopwatchLaps.innerHTML = `<div class="stopwatch-lap">
-      <div class="lap-number">Lap ${lapCount}</div>
+      <div id="stopwatch-lap-${lapCount}" class="lap-number">Lap ${lapCount}</div>
       <div id="lap-time-1" class="lap-time">00:00:00.00</div>
     </div>`;
 });
 
 lapButton.addEventListener('click', function addLapTime() {
-  console.log(lapDifference);
   lapCount++;
+  lapTimes.push(10 * (lapTimeNumber - 1));
   lapTimeNumber = 0;
   stopwatchLaps.innerHTML =
-    `<div class="stopwatch-lap">
+    `<div id="stopwatch-lap-${lapCount}" class="stopwatch-lap">
     <div class="lap-number">Lap ${lapCount}</div>
     <div id="lap-time-${lapCount}" class="lap-time">${msToTime(10 * lapTimeNumber)}</div>
   </div>` + stopwatchLaps.innerHTML;
+  changeLapColors();
 });
 
-let lapDifference = `${msToTime(
-  timeToMs(stopwatchTime.innerHTML) - timeToMs(document.querySelector(`#lap-time-${lapCount}`).innerHTML)
-)}`;
-
-function timeToMs(timeString) {
-  let timeArray = timeString.split(':');
-  return 1000 * (parseInt(timeArray[0] * 3600) + parseInt(timeArray[1] * 60) + parseFloat(timeArray[2]));
+function changeLapColors() {
+  for (let i = 1; i < lapCount; i++) {
+    document.querySelector(`#stopwatch-lap-${i}`).style.color = 'white';
+  }
+  if (lapCount > 2) {
+    document.querySelector(`#stopwatch-lap-${1 + lapTimes.indexOf(Math.max(...lapTimes))}`).style.color = 'red';
+    document.querySelector(`#stopwatch-lap-${1 + lapTimes.indexOf(Math.min(...lapTimes))}`).style.color = 'green';
+  }
 }
 
 function msToTime(duration) {
