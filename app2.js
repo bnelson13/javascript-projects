d38a3f1cb2eb624bf0969c678069745baa64d89d
Brad Nelson <bnelson13@gmail.com>
2020-12-11 13:31:11 -0800

    Now working on getting lap time difference

--- app2.js
+++ app2.js
@@ -6,7 +6,7 @@ const stopwatchLaps = document.querySelector('.stopwatch-laps');
 const stopwatchTime = document.querySelector('#stopwatch-time');
 let timerInerval;
 lapTimes = [];
-lapCount = 0;
+lapCount = 1;
 let timeNumber = 0;
 
 startButton.addEventListener('click', function startTime() {
@@ -16,6 +16,7 @@ startButton.addEventListener('click', function startTime() {
   lapButton.style.display = 'block';
   timerInerval = setInterval(() => {
     stopwatchTime.innerHTML = msToTime(10 * timeNumber++);
+    document.querySelector(`#lap-time-${lapCount}`).innerHTML = msToTime(10 * timeNumber++ - 1);
   }, 10);
 });
 
@@ -28,26 +29,28 @@ stopButton.addEventListener('click', function stopTime() {
 });
 
 resetButton.addEventListener('click', function resetStopwatch() {
-  lapCount = 0;
+  lapCount = 1;
   stopwatchTime.innerHTML = '00:00:00.00';
   timeNumber = 0;
   stopwatchLaps.innerHTML = `<div class="stopwatch-lap">
       <div class="lap-number">Lap 1</div>
-      <div class="lap-time">00:00:00.00</div>
+      <div id="lap-time-1" class="lap-time">00:00:00.00</div>
     </div>`;
 });
 
 lapButton.addEventListener('click', function addLapTime() {
   lapCount++;
-  console.log(stopwatchTime.innerHTML, msToTime(getMsDifference(stopwatchTime.innerHTML)));
-  stopwatchLaps.innerHTML =
-    `<div class="stopwatch-lap">
+  console.log(stopwatchTime.innerHTML, msToTime(timeToMs(stopwatchTime.innerHTML)));
+  if (lapCount != 1) {
+    stopwatchLaps.innerHTML =
+      `<div class="stopwatch-lap">
     <div class="lap-number">Lap ${lapCount}</div>
     <div id="lap-time-${lapCount}" class="lap-time">${stopwatchTime.innerHTML}</div>
   </div>` + stopwatchLaps.innerHTML;
+  }
 });
 
-function getMsDifference(timeString) {
+function timeToMs(timeString) {
   let timeArray = timeString.split(':');
   return 1000 * (parseInt(timeArray[0] * 3600) + parseInt(timeArray[1] * 60) + parseFloat(timeArray[2]));
 }
